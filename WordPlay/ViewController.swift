a92f5aa99558f9dbd755ec155a1b32b70e9bdf9a
Ian W. Howe <ihowe6189@bg-mmcte-09.d214.org>
2015-11-06 11:59:57 -0600

    Initial Commit

---
+++ WordPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordPlay
+//
+//  Created by Ian W. Howe on 11/6/15.
+//  Copyright © 2015 Ian W. Howe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
